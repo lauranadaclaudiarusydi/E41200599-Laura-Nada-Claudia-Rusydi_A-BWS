66196aa5aa9319f280618e70827d0c6bea6d1dba
Laura Nada Claudia Rusydi <74998643+lauranadaclaudiarusydi@users.noreply.github.com>
2021-03-30 13:36:13 +0700

    latihan.java

---
+++ Minggu Ke-4/Latihan/do_while.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author AUDY
+ */
+public class do_while {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           // untuk membuat variabel
+        int i = 0;
+        
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+    }
+    
+}
